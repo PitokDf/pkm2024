69eccd0db4a519f1bb0da9196a838a27258b5efe
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-04-30 12:23:03 +0700

    bla

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function next(current){
     }while(nextLv == current);
     window.location.href = `${nextLv}.html`;
 }
+function start(){
+    nextLv = Math.floor(Math.random() * 4)+1;
+    window.location.href = `level/${nextLv}.html`;
+}
